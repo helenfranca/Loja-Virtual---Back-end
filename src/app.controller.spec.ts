b03d2b0bdc302176e76239f3551a84cf271ebe27
unknown <you@example.com>
2019-04-09 22:52:52 -0300

    Crud: pessoa

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,10 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Sistema de convocação de doação de Sangue!"', () => {
+      expect(appController.getHello()).toBe(
+        'Sistema de convocação de doação de Sangue!',
+      );
     });
   });
 });
