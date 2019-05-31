8b9630370bcbd26810875744e424fa57001e1aba
Your Name <you@example.com>
2019-05-31 18:30:49 -0300

    Conclusão da remodulagem da arquitetura do projeto

--- src/doacaodesangue/service/doador.service.ts
+++ src/doacaodesangue/service/doador.service.ts
@@ -1,7 +1,6 @@
 import { genericInterface } from './interface/generic.interface';
 import { Injectable } from '@nestjs/common';
 import { Doador } from '../model/doador.entity';
-import { Pessoa } from '../model/pessoa.entity';
 
 @Injectable()
 export class DoadorService implements genericInterface<Doador> {
