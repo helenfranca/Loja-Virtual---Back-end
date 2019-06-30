317ed27f48909a8fa35fe173e96cf7119f489c5c
Your Name <you@example.com>
2019-06-30 14:42:59 -0300

    Versão login

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,34 @@
+import { Module, CacheModule } from '@nestjs/common';
+import { PessoaService } from 'src/doacaodesangue/service/pessoa.service';
+import { databaseProviders } from 'src/doacaodesangue/database/database.providers';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './controller/auth.controller';
+import { AuthService } from './service/auth.service';
+import { GoogleStrategy } from './service/google.strategy';
+import { AdministradorService } from 'src/doacaodesangue/service/administrador.service';
+
+const modelProvider = [...databaseProviders];
+
+const modelService = [
+  PessoaService,
+  AuthService,
+  GoogleStrategy,
+  AdministradorService,
+];
+
+const modelController = [AuthController];
+require('dotenv').config();
+@Module({
+  imports: [
+    CacheModule.register({
+      ttl: 10,
+      max: 10,
+    }),
+    JwtModule.register({
+      secretOrPrivateKey: process.env.PRIVATE_KEY,
+    }),
+  ],
+  providers: [...modelProvider, ...modelService],
+  controllers: [...modelController],
+})
+export class AuthModule {}
